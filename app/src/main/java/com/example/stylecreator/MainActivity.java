e45a0b0f0fc0ed69a9a1184b9ae825d475888a0c
CamdenKSeaver <Camdenkseaver@gmail.com>
2022-09-07 08:19:37 -0500

    started colors

---
+++ app/src/main/java/com/example/stylecreator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.stylecreator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
