e37ba44d79b1f5eacc83bc13334f814fe72a38e2
CamdenKSeaver <Camdenkseaver@gmail.com>
2022-09-08 08:19:00 -0500

    Added text and button

--- app/src/main/java/com/example/stylecreator/MainActivity.java
+++ app/src/main/java/com/example/stylecreator/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.stylecreator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    EditText text;
+    TextView name;
+
+    public int count(String str){
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.substring(i,i+1).equals(" ")){
+
+            }
+                else
+                    count++;
+
+        }
+        return count;
+    }
+    public void countLetters(View view){
+     //text = findViewById(R.id.namet);
+       name = findViewById(R.id.answer);
+       // int count = count((String) text.getText());
+      //  name.setText("Your Name, " + text.getText() + " has " + count + " Letters");
+
+        name.setText("camden");
+
     }
 }
